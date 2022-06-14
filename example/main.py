9ad640854067fade3bf2c671fa02d3c1ec7bd0a7
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-14 14:07:44 -0400

    Update main.py

--- example/main.py
+++ example/main.py
@@ -9,34 +9,26 @@ def describeDataset(data):
     print(data.describe(include = "all"))
 
 def deleteEmpty(data):
-    for x in data.index:
+    for x in data.index: #Loops through all entries in the DataFrame, deleting all rows which the number of fires is equal to 0.
         if(data.loc[x, "number"] == 0):
             data.drop(labels = [x], axis = 0, inplace = True)
         
     
 def createSubset(data):
-    fpm = data.groupby('month')['number'].sum()
-    months_unique = list(data.month.unique())
-    fpm = fpm.reindex(months_unique, axis=0)
-    fpm = fpm.to_frame()
-    fpm.reset_index(level = 0, inplace = True)
+    fpm = data.groupby('month')['number'].sum() #Creates the subset containing the months and the amount of fires per month.
+    months_unique = list(data.month.unique()) #Creates a series of the unique months
+    fpm = fpm.reindex(months_unique, axis=0) #reorders the months in the correct order, instead of alphabetically.
+    fpm = fpm.to_frame() #Converts the Series to a DataFrame.
+    fpm.reset_index(level = 0, inplace = True) #Resets the index to 0-11 instead of using the Months as the index.
     translateMonths(data)
 
 def translateMonths(data):
-    translator = Translator() #create an object of Translator 
-    for month in months_unique: 
-        detected = translator.detect(month)     
-        translated = translator.translate(month)     
-        print(detected)     
-        print(translated)     
-        print("...")
-        
-    translator2 = Translator() #create a another new object of translator
-    for x, m in enumerate(fpm['month']):
-        translated = translator2.translate(m)  
-        month1 = translated.text    
-        fpm.at[x, 'month'] = month1
-    print(fpm)
+    translator = Translator() #create a  new object of translator
+    for x, m in enumerate(fpm['month']): #For every index, x, and month, m.
+        translated = translator.translate(m)  #Translates the month, m, from portuguese to english
+        month1 = translated.text    #Saves the value of translated to a variable
+        fpm.at[x, 'month'] = month1 #Sets the value at index x, to the correct translated value
+    printDataset(fpm) #Prints the subset
 
 #Call functions below this point 
 try: 
