2486b48ce0a57020673e25656ce87a6d07b9cad9
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-14 09:38:07 -0400

    Update pandasPractice.py

--- pandasPractice.py
+++ pandasPractice.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
-df = pd.read_csv('data.csv')
-
 
 #Basic Pandas functions
 
-print(df.info()) #Prints informtaion about the dataset, including number of columns, rows, data types, and non null counts.
-print(df.head(10)) #Prints the first 10 entries in the DataFrame.
-print(df.tail(10)) #Prints the last 10 entries in the DataFrame.
+
+def basicFunctionsExample():
+  df = pd.read_csv('data.csv') #Reads the comma seperated value file and transforms it into a Pandas DataFrame named df.
+  #This dataset was obtained from W3Schools and can be found on their website.
+  
+  print(df.info()) #Prints informtaion about the dataset, including number of columns, rows, data types, and non null counts.
+  print(df.head(10)) #Prints the first 10 entries in the DataFrame.
+  print(df.tail(10)) #Prints the last 10 entries in the DataFrame.
+
+  
+#Cleaning datasets
+def cleaningExample():
+    
+#Call any functions below this line in order to avoid non defined function errors during compilation.
