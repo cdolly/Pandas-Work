53154c0b083650a1475b7874ce77b6ec6696153e
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-14 10:08:55 -0400

    Update pandasPractice.py

--- pandasPractice.py
+++ pandasPractice.py
@@ -16,19 +16,30 @@ def basicFunctionsExample():
 
   
 #Cleaning datasets
-def cleaningExample():
+def cleaningExample(num): #Num represents which example you'd like to view.
+  
+  if(num < 1 or num > 8):
+    print("Please choose a correct example to view")
+    return
+ 
   df = pd.read_csv('data.csv') #Reads the comma seperated value file and transforms it into a Pandas DataFrame named df.
   #This dataset was obtained from W3Schools and can be found on their website.
+    
+  if(num == 1):
+    new_df = df.dropna() #Creates a new DataFrame with the empty cells and their rows removed.
+    print(new_df.to_string()) #Prints the new DataFrame.
   
-  new_df = df.dropna() #Creates a new DataFrame with the empty cells and their rows removed.
-  print(new_df.to_string()) #Prints the new DataFrame.
-  
-  df.fillna(130, inplace = True) #Replaces empty values in the DataFrame with the value 130, does not return a NEW DataFrame, only changes the original.
-  
-  df["Calories"].fillna(130, inplace = True) #Replaces empty values in the "Calories" column with the value 130, does not return a new DataFrame.
-  
-  x = df["Calories"].mean() #Creates a variable, X, that is the mean of all values in the "Calories" column. The same can be done with median and mode funtions.
-
-  df["Calories"].fillna(x, inplace = True) #Replaces all empty cells in the "Calories" column with the value of x, which is the average of the "Calories" column.
+  if(num == 2):
+    df.fillna(130, inplace = True) #Replaces empty values in the DataFrame with the value 130, does not return a NEW DataFrame, only changes the original.
+    print(df.to_string()) #Prints the modified DataFrame.
+    
+  if(num == 3):
+    df["Calories"].fillna(130, inplace = True) #Replaces empty values in the "Calories" column with the value 130, does not return a new DataFrame.
+    print(df.to_string()) #Prints the modified DataFrame.
+    
+  if(num == 4):
+    x = df["Calories"].mean() #Creates a variable, X, that is the mean of all values in the "Calories" column. The same can be done with median and mode funtions.
+    df["Calories"].fillna(x, inplace = True) #Replaces all empty cells in the "Calories" column with the value of x, which is the average of the "Calories" column.
+    print(df.to_string()) #Prints the modified DataFrame.
     
 #Call any functions below this line in order to avoid non defined function errors during compilation.
