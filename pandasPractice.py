27d7be5ab4410cf361d883b868d5f628dc89b170
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-14 09:30:20 -0400

    Update pandasPractice.py

--- pandasPractice.py
+++ pandasPractice.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
-print(df.info())
-print(df.head(10))
-print(df.tail(10))
+print(df.info()) #Prints informtaion about the dataset, including number of columns, rows, data types, and non null counts.
+print(df.head(10)) #Prints the first 10 entries in the DataFrame.
+print(df.tail(10)) #Prints the last 10 entries in the DataFrame.
