02c8f1d1e7f596d956f1be3ff76222bc8370aad8
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-14 13:03:23 -0400

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,1 +1,13 @@
 import pandas as pd
+
+try:
+    df = pd.read_csv('amazon.csv', thousands = '.')
+except:
+    print("File not found")
+
+#Initial dataset
+#print(df.to_string())
+
+#Checking for empty entries
+print(df.isna().sum())
+
